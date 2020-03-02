d1acbf541ac7cff534c3a42b04f3fc5cc5d8e002
namitjuneja <juneja.namit@gmail.com>
2020-03-01 21:10:24 -0500

    added layerwise padding function

--- code/helper.py
+++ code/helper.py
@@ -23,84 +23,84 @@ def get_graph_from_file(phi, chi, replica, source_dir=None):
 
 # get max degree node 
 def get_max_degree_node_color(graph):
-    
-    # Step 1: Filter all the black nodes and choose one as default.
-    #black_nodes = list(filter(lambda x: x[1]['color'] == 0, graph.nodes(data=True)))
-    black_nodes = list(graph.nodes(data=True))
-    max_degree_node = black_nodes[0][0]
-    
-    # Step 2: Iterate over the rest of the nodes and find the most connected black node. 
-    for node in black_nodes:
-        if graph.degree[node[0]] > graph.degree[max_degree_node]:
-            max_degree_node = node[0]
-        elif graph.degree[node[0]] == graph.degree[max_degree_node]:
-            if node[1]['area'] > graph.nodes(data=True)[max_degree_node]['area']:
-                max_degree_node = node[0]
+	
+	# Step 1: Filter all the black nodes and choose one as default.
+	#black_nodes = list(filter(lambda x: x[1]['color'] == 0, graph.nodes(data=True)))
+	black_nodes = list(graph.nodes(data=True))
+	max_degree_node = black_nodes[0][0]
+	
+	# Step 2: Iterate over the rest of the nodes and find the most connected black node. 
+	for node in black_nodes:
+		if graph.degree[node[0]] > graph.degree[max_degree_node]:
+			max_degree_node = node[0]
+		elif graph.degree[node[0]] == graph.degree[max_degree_node]:
+			if node[1]['area'] > graph.nodes(data=True)[max_degree_node]['area']:
+				max_degree_node = node[0]
 
-    return max_degree_node
+	return max_degree_node
 
 
 # priority bfs
 def priority_bfs(graph, root):
-    vector  = []
-    visited = []
-    queue   = []
-    
-    visited.append(root)
-    queue.append((root,graph.nodes[root]['area']/root,graph.nodes[root]['perimeter']))
-    
-    while queue:
-        
-        # Step A: Dequeue it to the vector
-        current_node = queue.pop(0)
-        
-        
-        # Step B: Append it to the vector
-        vector.append(((-1)**(graph.nodes[current_node[0]]['color']+1)) * (current_node[1]))
-        #vector.append(current_node[1])
-        #vector.append(graph.nodes[current_node[0]]['area'])
-        
-        #set_trace()
-        
-        # Step C: Get all of elements children 
-        # Storage format : [ (<node>, <node_area>) , (<node>, <node_area>), ... ]
-        current_node_neighbors = []
-        for neighbor in graph.neighbors(current_node[0]):
-            volume_to_surface_ratio = graph.nodes()[neighbor]['area'] / graph[current_node[0]][neighbor]['weight']
-            current_node_neighbors.append((neighbor, volume_to_surface_ratio))
-        
-        #current_node_neighbors = [(neighbor, graph.nodes()[neighbor]['area']) for neighbor in graph.neighbors(current_node[0])]
-        
-            
-        # Step D: Sort them by their area and enqueue them
-        # sorting
-        current_node_neighbors.sort(key = lambda x: x[1])
-        # enqueueing
-        # make sure that node has not been visited first
-        # althugh that should not happen since the graph is
-        # always acyclic
-        for neighbor in current_node_neighbors:
-            if neighbor[0] not in visited:
-                visited.append(neighbor[0])
-                queue.append(neighbor)
-    
-    vector = np.array(vector)          
-    return vector      
+	vector  = []
+	visited = []
+	queue   = []
+	
+	visited.append(root)
+	queue.append((root,graph.nodes[root]['area']/root,graph.nodes[root]['perimeter']))
+	
+	while queue:
+		
+		# Step A: Dequeue it to the vector
+		current_node = queue.pop(0)
+		
+		
+		# Step B: Append it to the vector
+		vector.append(((-1)**(graph.nodes[current_node[0]]['color']+1)) * (current_node[1]))
+		#vector.append(current_node[1])
+		#vector.append(graph.nodes[current_node[0]]['area'])
+		
+		#set_trace()
+		
+		# Step C: Get all of elements children 
+		# Storage format : [ (<node>, <node_area>) , (<node>, <node_area>), ... ]
+		current_node_neighbors = []
+		for neighbor in graph.neighbors(current_node[0]):
+			volume_to_surface_ratio = graph.nodes()[neighbor]['area'] / graph[current_node[0]][neighbor]['weight']
+			current_node_neighbors.append((neighbor, volume_to_surface_ratio))
+		
+		#current_node_neighbors = [(neighbor, graph.nodes()[neighbor]['area']) for neighbor in graph.neighbors(current_node[0])]
+		
+			
+		# Step D: Sort them by their area and enqueue them
+		# sorting
+		current_node_neighbors.sort(key = lambda x: x[1])
+		# enqueueing
+		# make sure that node has not been visited first
+		# althugh that should not happen since the graph is
+		# always acyclic
+		for neighbor in current_node_neighbors:
+			if neighbor[0] not in visited:
+				visited.append(neighbor[0])
+				queue.append(neighbor)
+	
+	vector = np.array(vector)          
+	return vector      
 
 
 # generate vectors
 def generate_vectors(graphs):
-    vectors = []
+	vectors = []
 
-    for graph in graphs:
-        # set the node with the highest edges as root
-        root = get_max_degree_node_color(graph)
-    
-        # get BFS vector
-        vector = priority_bfs(graph, root)
-        vectors.append(vector)
-    
-    return vectors
+	for graph in graphs:
+		# set the node with the highest edges as root
+		root = get_max_degree_node_color(graph)
+	
+		# get BFS vector
+		vector = priority_bfs(graph, root)
+		vectors.append(vector)
+	
+	return vectors
 
 
 # plot distance matrix
@@ -138,79 +138,97 @@ def plot_distance_graph(D, compare_with, start_slice):
 
 # front pad vector
 def front_pad(vector, max_dimension):
-    # make no array if not
-    if not isinstance(vector, np.ndarray): vector=np.array(vector)
+	# make no array if not
+	if not isinstance(vector, np.ndarray): vector=np.array(vector)
 
-    return np.pad(vector, (0,max_dimension-len(vector)))
+	return np.pad(vector, (0,max_dimension-len(vector)))
 
 
-# # generate padded vectors
+# generate padded vectors
 def generate_padded_vectors(vectors):
-    split_vectors = []
+	split_vectors = []
 
-    for vector in vectors:
-        # split the nodes based on sign
-        split_indices = []
+	for vector in vectors:
+		# split the nodes based on sign
+		split_indices = []
 
-        for index,d in enumerate(vector):
-            if d >= 0:
-                split_indices.extend([index, index+1])
+		for index,d in enumerate(vector):
+			if d >= 0:
+				split_indices.extend([index, index+1])
 
-        split_vector = np.split(vector, split_indices)
-        split_vectors.append(split_vector)
-
-
-
-    # get the maximum length of split at each 
-    # position for all the vectors
-    max_split_length = {}
-
-    for split_vector in split_vectors:
-        for index, split in enumerate(split_vector):
-            max_split_length[index] = max([len(split), max_split_length.get(index, 0)])
+		split_vector = np.split(vector, split_indices)
+		split_vectors.append(split_vector)
 
 
 
-    # pad all the splits to their 
-    # respective max lengths
-    padded_split_vectors = []
-    for split_vector in split_vectors:
+	# get the maximum length of split at each 
+	# position for all the vectors
+	max_split_length = {}
 
-        padded_split_vector = []
-        for index,split in enumerate(split_vector):
-
-            padded_split = front_pad(split, max_split_length[index])
-            padded_split_vector.append(padded_split)
-
-        padded_split_vectors.append(padded_split_vector)
+	for split_vector in split_vectors:
+		for index, split in enumerate(split_vector):
+			max_split_length[index] = max([len(split), max_split_length.get(index, 0)])
 
 
 
-    # Merge all splits into single vector
-    merged_vectors = []
-    for padded_split_vector in padded_split_vectors:
-        merged_vector = np.concatenate(padded_split_vector)
-        merged_vectors.append(merged_vector)
+	# pad all the splits to their 
+	# respective max lengths
+	padded_split_vectors = []
+	for split_vector in split_vectors:
+
+		padded_split_vector = []
+		for index,split in enumerate(split_vector):
+
+			padded_split = front_pad(split, max_split_length[index])
+			padded_split_vector.append(padded_split)
+
+		padded_split_vectors.append(padded_split_vector)
 
 
-    # over all frontpad to compensate for
-    # different number of layers in each graph
-    max_dimension = max(map(len, merged_vectors))
 
-    padded_vectors = []
-    for merged_vector in merged_vectors:
-
-        padded_vector = front_pad(merged_vector, max_dimension)
-        padded_vectors.append(padded_vector)
+	# Merge all splits into single vector
+	merged_vectors = []
+	for padded_split_vector in padded_split_vectors:
+		merged_vector = np.concatenate(padded_split_vector)
+		merged_vectors.append(merged_vector)
 
 
-    # make all vector dimension magnitudes 
-    # positive irrespective of color
-    positive_vectors = []
-    for padded_vector in padded_vectors:
+	# over all frontpad to compensate for
+	# different number of layers in each graph
+	max_dimension = max(map(len, merged_vectors))
 
-        positive_vector = np.abs(padded_vector)
-        positive_vectors.append(positive_vector)
+	padded_vectors = []
+	for merged_vector in merged_vectors:
 
-    # write some tests maybe
-    return positive_vectors
+		padded_vector = front_pad(merged_vector, max_dimension)
+		padded_vectors.append(padded_vector)
+
+
+	# make all vector dimension magnitudes 
+	# positive irrespective of color
+	positive_vectors = []
+	for padded_vector in padded_vectors:
+
+		positive_vector = np.abs(padded_vector)
+		positive_vectors.append(positive_vector)
+
+	# write some tests maybe
+	return positive_vectors
+
+
+
+# generate distance matrix
+def generate_distance_matrix(vectors):
+	vector_count = len(vectors)
+
+	D = np.empty((vector_count, vector_count))
+	for i in range(2):
+		for j in range(2):
+			vector_pair = [vectors[i],vectors[j]]
+			vector_pair_2 = vector_pair[::-1]
+			padded_vectors = generate_padded_vectors(vector_pair)
+			padded_vectors_2 = generate_padded_vectors(vector_pair_2)
+			if (padded_vectors[0] != padded_vectors_2[0]).any() or (padded_vectors[1] != padded_vectors_2[1]).any():
+				print(i,j,(padded_vectors[0] != padded_vectors_2[0]))
+				return(padded_vectors[0], padded_vectors_2[0])
+				print("Not working")
